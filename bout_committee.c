189dd0fa4de8c67ba693abdb4fea6bb1380f98a4
JFreud <jrm.frdnbrg@gmail.com>
2018-01-04 16:13:30 -0500

    copy paste handshake code

---
+++ bout_committee.c
@@ -0,0 +1,47 @@
+#include "network.h"
+
+void process(char *s);
+void subserver(int from_client);
+
+static void sighandler(int signo) {
+  if (signo == SIGINT) {
+    remove("luigi");
+    exit(0);
+  }
+}
+
+int main() {
+  while (1) {
+    signal(SIGINT, sighandler);
+    int from_client;
+    from_client = server_setup();
+    if(from_client){
+      int f = fork();
+      if (f) {
+        printf("forked!\n");
+      } //parent
+      else {//child
+        subserver(from_client);
+      }
+    }
+  }
+}
+
+void subserver(int from_client) {
+  int to_client = server_connect(from_client);// handshake should be finished
+  char buffer[BUFFER_SIZE];
+
+  while(read(from_client, buffer, sizeof(buffer))) {
+    process(buffer);
+    printf("[server] performed process: %s\n", buffer);
+    write(to_client, buffer, sizeof(buffer));
+  }
+  exit(0);
+}
+
+void process(char * s) {
+  while (*s) { //make caps
+    *s = toupper(*s);
+    s++;
+  }
+}
---
+++ network.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <ctype.h>
+#include <time.h>
+
+
+#define HANDSHAKE_BUFFER_SIZE 10
+#define BUFFER_SIZE 1000
+
+
+//fxn headers:...
